56c9c08ebef5a946cd359d76041bcc55af5188ea
Ranayke Boni Ferreira <ranayke2009@gmail.com>
2022-12-19 16:45:57 -0300

    feat(hero): Refactoring API code

--- src/App.tsx
+++ src/App.tsx
@@ -4,8 +4,6 @@ import axios from 'axios';
 
 import { URL, APISTRING } from './data/constants';
 
-//import * as CONST from './data/constants';
-
 import Hero from './components/Hero';
 import NavBar from './components/NavBar';
 import Carousel from './components/Carousel';
@@ -14,28 +12,21 @@ import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
 
 const App = () => {
-  //const { URL, APISTRING } = CONST;
-  
   const [ movies, setMovies ] = useState<any[]>([]);
   const url = `${URL}/discover/movie${APISTRING}&sort_by=popularity.desc`
 
-  const fetchData = async () => {
-  try {
-      //const response = await fetch(`${URL}/discover/movie${APISTRING}&sort_by=popularity.desc`);
-      //const data = await response.json();
-      //setMovies(data.results);
-      const response = await axios.get(url);
-      setMovies(response.data.results);
-  } catch (error) {
-    setMovies([])
-  }
-}
-
   useEffect(() => {
-    fetchData();
-  }, []);
+    const fetchData = async () => {
+      try {
+        const response = await axios.get(url);
+        setMovies(response.data.results);
+      } catch (error) {
+        setMovies([])
+      }
+    }
 
-  // useEffect(() => movies && console.log(movies), [ movies ])
+    fetchData();
+  }, [url]);
 
   return (
     <div className="m-auto antialiased font-sans bg-black text-white">
